4e5810a6dabaf2c3617df2ee8c4e01f47105030c
kibakin <kibakin@snu.ac.kr>
2015-09-24 07:02:52 +0000

    final commit

--- telephonedirectory.rb
+++ telephonedirectory.rb
@@ -15,8 +15,10 @@ loop do
     
     directory << {name: name, phonenumber: phonenumber, gender: gender}
     
-    
-    puts directory
+    directory.each_with_index do |x, index|
+      
+        puts "#{index+1}. 이름:#{x[:name]} 전화번호:#{x[:phonenumber]} 성별:#{x[:gender]}"
+    end
     
 break if name == "exit"
 end
