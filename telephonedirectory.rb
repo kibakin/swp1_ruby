e48aa6e60ac7e84c848a1db3d6bb48fcf14de1bb
kibakin <kibakin@snu.ac.kr>
2015-09-24 03:00:02 +0000

    second commit

--- telephonedirectory.rb
+++ telephonedirectory.rb
@@ -1,2 +1,19 @@
 print "input your name"
 name = gets.chomp
+
+print "input your phone number"
+phonenumber = gets.chomp
+
+print "input your gender"
+gender = gets.chomp
+
+if gender == "male"
+    gender == "male"
+elsif gender =="female"
+    gender == "female"
+else
+   gender == "male"
+end
+
+directory = [name, phonenumber, gender]
+print directory
